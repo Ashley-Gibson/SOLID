f6aaa5f68ae50f1bb4123b9c343f5e488677c4d6
ashleydgibson <ashley_gibson@ajg.com>
2020-01-30 13:11:21 +0000

    task/SOLID-79 - Initial commit laying out comments for future functionality of this feature

--- SOLID/Single Responsibility Principle/Program.cs
+++ SOLID/Single Responsibility Principle/Program.cs
@@ -4,6 +4,13 @@
     {
         static void Main(string[] args)
         {
+            // Give User Message to select a vehicle to output data for
+
+            // Get data from the correct vehicle
+
+            // Output data to the console
+
+            // Hang the console and loop until the user quits
         }
     }
 }
