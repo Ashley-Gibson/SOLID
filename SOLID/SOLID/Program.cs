562c0abe54583af7a8b3bdc5e0c8c4acf25c4e7a
ashleydgibson <ashley_gibson@ajg.com>
2020-02-06 11:31:37 +0000

    task/SOLID-86 - Mirrored SRP functionality in OCP before refactoring to ensure Open/Closed Principle is followed in OCP Project only

--- SOLID/SOLID/Program.cs
+++ SOLID/SOLID/Program.cs
@@ -1,14 +1,42 @@
-﻿namespace Open_Closed_Principle
+﻿using System;
+
+namespace Open_Closed_Principle
 {
     class Program
     {
+        enum Status
+        {
+            Running,
+            Finished
+        };
+
         static void Main(string[] args)
         {
             // Allow User to choose a type of Vehicle
 
             // Ensure the Vehicle interface allows you to choose a different type of Vehicle (Plane/Car/Motorbike... etc) without having to change the functionality of any of the existing classes
-                // Include Switch statement to choose different vehicles (make it easily extendible)
+            // Include Switch statement to choose different vehicles (make it easily extendible)
             
+            int vehicleNumberFromUserInput = 0;
+
+            while (vehicleNumberFromUserInput != -1)
+            {
+                vehicleNumberFromUserInput = PrepareConsole();
+
+                Vehicle vehicle = VehicleManager.GetVehicleDetails(vehicleNumberFromUserInput);
+                string OutputString = VehicleManager.ConstructOutputString(vehicle);
+                VehicleManager.OutputVehicleDetails(OutputString);
+            }
+        }
+
+        private static int PrepareConsole()
+        {
+            Console.WriteLine("####################");
+
+            Console.WriteLine("\nWhat vehicle would you like to view?\n");
+            string UserInput = Console.ReadLine();
+
+            return int.TryParse(UserInput, out int UserInputNumber) ? UserInputNumber : -1;
         }
     }
 }
---
+++ SOLID/SOLID/VehicleManager.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Open_Closed_Principle
+{
+    public static class VehicleManager
+    {
+        private readonly static List<Vehicle> allVehicles = new List<Vehicle>()
+        {
+            new Car()
+            {
+                VehicleID = 0,
+                Make = "Ford",
+                Model = "Focus",
+                Price = 6000m,
+                Mileage = 10000
+            },
+            new Car()
+            {
+                VehicleID = 1,
+                Make = "Vauxhall",
+                Model = "Corsa",
+                Price = 7000m,
+                Mileage = 18000
+            },
+            new Motorbike()
+            {
+                VehicleID = 2,
+                Make = "Honda",
+                Model = "VFR",
+                Price = 4500m,
+                Mileage = 2000
+            },
+            new Motorbike()
+            {
+                VehicleID = 3,
+                Make = "Kawasaki",
+                Model = "Ninja",
+                Price = 5500m,
+                Mileage = 3200
+            },
+            new Plane()
+            {
+                VehicleID = 4,
+                Make = "Boeing",
+                Model = "747",
+                Price = 2700000m               
+            },
+            new Plane()
+            {
+                VehicleID = 5,
+                Make = "Airbus",
+                Model = "A300",
+                Price = 2100000m
+            }
+        };
+
+        public static Vehicle GetVehicleDetails(int vehicleNumberFromUserInput)
+        {
+            IEnumerable<Vehicle> returnedVehicle = allVehicles.Where(v => v.VehicleID == vehicleNumberFromUserInput);
+
+            return returnedVehicle.Any() 
+                ? returnedVehicle.FirstOrDefault()
+                : new UnknownVehicle() { };
+        }
+
+        public static void OutputVehicleDetails(string OutputString)
+        {           
+            Console.WriteLine(OutputString);
+        }
+
+        public static string ConstructOutputString(Vehicle vehicle)
+        {
+            string OutputString = "\n####################\n";
+
+            if (vehicle.GetType() != typeof(UnknownVehicle))
+            {
+                OutputString +=
+                    $"Make: {vehicle.Make}\n" +
+                    $"Model: {vehicle.Model}\n" +
+                    $"Price: £{vehicle.Price}\n" +
+                    $"Wheels: {vehicle.Wheels}\n" +
+                    $"Mileage: {vehicle.Mileage}\n";
+            }
+            else
+            {
+                OutputString += $"{vehicle.ErrorMessage}\n";
+            }
+
+            return OutputString;
+        }
+    }
+}
